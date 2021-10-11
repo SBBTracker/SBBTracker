9a800714375b1ea81d01894fe71fd11e73bc4077
Andrew Rasch <andrewarasch@gmail.com>
2021-10-10 21:43:36 -0400

    Added first pass gui
    Updated LogParser to hook into gui
    Added resize script to make assets

--- src/LogParser.py
+++ src/LogParser.py
@@ -6,28 +6,311 @@ import re
 
 appdata = Path(os.environ["APPDATA"])
 logfile = appdata.parent.joinpath("LocalLow/Good Luck Games/Storybook Brawl/Player.log")
+offsetfile = appdata.parent.joinpath("LocalLow/Good Luck Games/Storybook Brawl/Player.log.offset")
 
-def parseLine(line):
-    if "Type:" in line:
-        trimmed = line[line.index('Type:'):]
-        splitlines = re.split("(\w+):", trimmed)
-        it = iter(splitlines[1:])
-        paired = list(zip(it, it))
-        cleaned = clean(paired)
-        return cleaned
+try:
+    os.remove(offsetfile)
+except:
+    pass
+
+VERYLARGE = 2 ** 20
+NOTFOUND = -1
+
+EVENT_CHARACTER = 'Character'
+EVENT_ADDPLAYER = 'GLG.Transport.Actions.ActionAddPlayer'
+EVENT_ATTACK = 'GLG.Transport.Actions.ActionAttack'
+EVENT_BRAWLCOMPLETE = 'GLG.Transport.Actions.ActionBrawlComplete'
+EVENT_CASTSPELL = 'GLG.Transport.Actions.ActionCastSpell'
+EVENT_CONNINFO = 'GLG.Transport.Actions.ActionConnectionInfo'
+EVENT_CREATECARD = 'GLG.Transport.Actions.ActionCreateCard'
+EVENT_DEALDAMAGE = 'GLG.Transport.Actions.ActionDealDamage'
+EVENT_DEATH = 'GLG.Transport.Actions.ActionDeath'
+EVENT_DEATHTRIGGER = 'GLG.Transport.Actions.ActionDeathTrigger'
+EVENT_ENTERBRAWLPHASE = 'GLG.Transport.Actions.ActionEnterBrawlPhase'
+EVENT_ENTERINTROPHASE = 'GLG.Transport.Actions.ActionEnterIntroPhase'
+EVENT_ENTERRESULTSPHASE = 'GLG.Transport.Actions.ActionEnterResultsPhase'
+EVENT_ENTERSHOPPHASE = 'GLG.Transport.Actions.ActionEnterShopPhase'
+EVENT_MODIFYGOLD = 'GLG.Transport.Actions.ActionModifyGold'
+EVENT_MODIFYLEVEL = 'GLG.Transport.Actions.ActionModifyLevel'
+EVENT_MODIFYNEXTLEVELXP = 'GLG.Transport.Actions.ActionModifyNextLevelXP'
+EVENT_MODIFYXP = 'GLG.Transport.Actions.ActionModifyXP'
+EVENT_MOVECARD = 'GLG.Transport.Actions.ActionMoveCard'
+EVENT_PLAYFX = 'GLG.Transport.Actions.ActionPlayFX'
+EVENT_PRESENTDISCOVER = 'GLG.Transport.Actions.ActionPresentDiscover'
+EVENT_PRESENTHERODISCOVER = 'GLG.Transport.Actions.ActionPresentHeroDiscover'
+EVENT_REMOVECARD = 'GLG.Transport.Actions.ActionRemoveCard'
+EVENT_ROLL = 'GLG.Transport.Actions.ActionRoll'
+EVENT_SLAYTRIGGER = 'GLG.Transport.Actions.ActionSlayTrigger'
+EVENT_SUMMONCHARACTER = 'GLG.Transport.Actions.ActionSummonCharacter'
+EVENT_UPDATECARD = 'GLG.Transport.Actions.ActionUpdateCard'
+EVENT_UPDATEEMOTES = 'GLG.Transport.Actions.ActionUpdateEmotes'
+EVENT_UPDATETURNTIMER = 'GLG.Transport.Actions.ActionUpdateTurnTimer'
+EVENT_SPELL = 'Spell'
+
+TASK_ADDPLAYER = "AddPlayer"
+TASK_GATHERIDS = "GatherIDs"
+TASK_GETROUND = "GetRound"
+TASK_GETROUNDGATHER = "GetRoundGather"
+TASK_ENDROUNDGATHER = "EndRoundGather"
+TASK_NEWGAME = "TaskNewGame"
+
+JOB_PLAYERINFO = "PlayerInfo"
+JOB_BOARDINFO = "BoardInfo"
+JOB_ROUNDINFO = "RoundInfo"
+JOB_NEWGAME = "StateNewgame"
+
+
+def parse_list(line, delimiter):
+    """
+    Transforms weird delimited sections of logfile into nice lists.
+    Can also be used to operate on badly formatted section of log that
+    are missing their pipes.
+
+    Parameters
+    ----------
+    line : str
+        The current line of text being operated on
+    delimiter : str
+        The delimiter we're expecting to see in the 
+        line separating list elements
+
+    Returns
+    -------
+    specval : list(str)
+        A list of values derived from the line
+    new_line : TYPE
+        The line, shortened after the point of the list
+
+    """
+    find = ':'
+    dis = line.find(find)
+    if dis == -1:
+        dis = len(line)
+    reverb = ''.join(reversed(line[:dis]))
+    lastpipe = reverb.find(delimiter)
+    items = line[:dis][:-lastpipe].split(delimiter)
+    specval = [i.strip() for i in items]
+
+    new_line = line[dis - lastpipe:].strip()
+
+    return new_line, specval
+
+
+def process_line(line, ifs, dt=None, path=[]):
+    """
+    A fun recursive function for turning a log line into a dictionary.
+    Log lines are kind of like flattened YAML, except they have mistakes
+    and have random newlines inside of them and are all around
+    a fantastic time. We handle those edge cases here.
+    
+    Parameters
+    ----------
+    line : str
+        The current line of text being operated on
+    ifs : input file stream
+        We need this when we find aberrant newlines...
+    dt : dict
+        This is where the state of the dictionary is being held. Exists
+        to be passed through the recursive function
+    path : list(str)
+        The ordered set of keys that bring us to our current "locatiom" in
+        the dict. There may be a better way to do this, I don't know
+    """
+    specval = None
+    brick = None
+
+    current_key = None if not path else path[-1]  # last item of path if exists
+
+    # State dictionary invocation
+    if dt is None:
+        lb_dt = lambda: defaultdict(lb_dt)
+        dt = defaultdict(lb_dt)
+
+    ### Get to the correct depth of the dictionary for state
+    _dt = dt
+    for p in path[:len(path) - 1]:
+        _dt = _dt[p]
+
+        ### If we're handling a list gather it here
+    if current_key in ['Keywords', 'Subtypes', 'ValidTargets']:
+        line, specval = parse_list(line=line, delimiter='|')
+        specval = specval[:-2]
+    ### If we need to hunt for the next val do it here
+    elif current_key in ['FrameOverride']:
+        line, specval = parse_list(line=line, delimiter=' ')
     else:
-        return []
+        # First find the distance to the first colon or pipe
+        # colon means we go "in" a level
+        # pipe means we go "up" a level
+        coldis = line.find(':')
+        pipedis = line.find('|')
 
-def clean(entries):
-    cleaned = {}
-    for i in range(len(entries)):
-        key = entries[i][0]
-        val = entries[i][1].strip().rstrip(" | ")
-        if " | " in val:
-            val = str(val).split(" | ")
-        cleaned[key] = val
-    return cleaned
+        if current_key == 'GameText':
+            coldis = VERYLARGE
 
-while True:
-    for line in Pygtail(str(logfile)):
-        print(parseLine(line))
+        ### Handle game text having newlines
+        ### By grabbing the next line and attaching it if we 
+        ### Can't find a colon OR a pipe
+        if coldis in [NOTFOUND, VERYLARGE] and pipedis in [NOTFOUND]:
+            if current_key == 'GameText':
+                line = line + ifs.next()
+                coldis = VERYLARGE
+                pipedis = line.find('|')
+
+        ### Make sure -1s are handled appropriately for minmath
+        if coldis == NOTFOUND:
+            coldis = VERYLARGE
+        if pipedis == NOTFOUND:
+            pipedis = VERYLARGE
+
+    if specval is not None:
+        val = specval
+    else:
+        chop = min(coldis, pipedis)
+        brick = line[:chop].strip()
+        line = line[chop + 1:]
+        val = brick
+
+    if specval is None and pipedis == coldis:
+        if path and val:
+            _dt[current_key] = val
+
+        return dt
+    if specval is not None or pipedis < coldis:
+        # If we have a special value, then add it to the value
+        # of the current key. If we've found a pipe before a colon
+        # then we also have a value to record.
+        if val:
+            _dt[current_key] = val
+
+        else:
+            # Frame overrides can have no discernible value sometimes
+            if current_key == 'FrameOverride':
+                _dt[current_key] = val
+
+        path = path[:-1]
+        process_line(line, ifs, dt, path)
+    else:
+        # we've found a colon, we must go deeper
+        process_line(line, ifs, dt, [*path, brick])
+
+    # base case
+    return dt
+
+
+def parse(ifs):
+    """
+    Parse the log file into workable dictionaries. A nice function to 
+    separate the business logic from the parsing logic
+
+    Parameters
+    ----------
+    ifs : Input file stream
+        The tail of the logfile being read in
+
+    Yields
+    ------
+    info : Dictionary
+        A log line, dynamically transformed into a dict object for
+        further processing
+
+    """
+    for line in ifs:
+        if 'NEW GAME STARTED' in line:
+            yield Action(info=None, newgame=True)
+        elif 'QueueActionRPC' in line:
+            chop_idx = line.find('-') + 1
+            line = line[chop_idx:]
+            info = process_line(line, ifs)
+            yield Action(info)
+
+
+class Action:
+    def __init__(self, info, newgame=False):
+        if newgame:
+            self.task = TASK_NEWGAME
+            return
+
+        if info is not None:
+
+            self.action_type = info['Action']['Type']
+            if self.action_type == EVENT_ADDPLAYER:
+                self.task = TASK_ADDPLAYER
+                self.displayname = info['DisplayName']
+                self.heroname = info['Hero']['Card']['DisplayName']
+                self.playerid = info['Hero']['Card']['PlayerId']
+                self.attrs = ['displayname', 'heroname', 'playerid']
+
+            elif self.action_type == EVENT_ENTERBRAWLPHASE:
+                self.task = TASK_GATHERIDS
+                self.player1 = info['Action']['FirstPlayerId']
+                self.player2 = info['Action']['SecondPlayerId']
+                self.attrs = ['player1', 'player2']
+
+            elif self.action_type == EVENT_CREATECARD:
+                self.task = TASK_GETROUNDGATHER
+                cardinfo = info['Action']['Card']['Card']
+
+                self.playerid = cardinfo['PlayerId']
+                self.cardname = cardinfo['DisplayName']
+                self.cardattack = cardinfo['Attack']
+                self.cardhealth = cardinfo['Health']
+                self.is_golden = cardinfo['IsGolden']
+                self.slot = cardinfo['Slot']
+                self.zone = cardinfo['Zone']
+                self.attrs = ['cardname', 'cardattack', 'cardhealth', 'is_golden', 'slot', 'zone']
+
+            elif self.action_type in [EVENT_BRAWLCOMPLETE, EVENT_SUMMONCHARACTER, EVENT_ATTACK, EVENT_DEALDAMAGE]:
+                self.task = TASK_ENDROUNDGATHER
+                self.attrs = []
+
+            elif self.action_type == EVENT_ENTERSHOPPHASE:
+                self.task = TASK_GETROUND
+                self.round = info['Round']
+                self.attrs = ['round']
+            else:
+                self.task = None
+                self.attrs = []
+
+    def __repr__(self):
+        return json.dumps({k: getattr(self, k) for k in ['task', *self.attrs]}, sort_keys=True, indent=4)
+
+
+class Update:
+    def __init__(self, job, state):
+        self.job = job
+        self.state = state
+
+
+def run(window):
+    inbrawl = False
+    current_round = None
+    lastupdated = dict()
+    while True:
+        ifs = Pygtail(str(logfile))
+        for action in parse(ifs):
+            if action.task == TASK_NEWGAME:
+                inbrawl = False
+                current_round = None
+                lastupdated = dict()
+
+                window.write_event_value(JOB_NEWGAME, Update(JOB_NEWGAME, None))
+            elif not inbrawl and action.task == TASK_ADDPLAYER:
+                window.write_event_value(JOB_PLAYERINFO, Update(JOB_PLAYERINFO, action))
+            elif not inbrawl and action.task == TASK_GATHERIDS:
+                inbrawl = True
+                brawldt = dict()
+                brawldt[action.player1] = list()
+                brawldt[action.player2] = list()
+                lastupdated[action.player1] = current_round
+                lastupdated[action.player2] = current_round
+            elif inbrawl and action.task == TASK_GETROUNDGATHER:
+                if action.zone in ['Spell', 'Treasure', 'Character']:
+                    brawldt[action.playerid].append(action)
+            elif inbrawl and action.task == TASK_ENDROUNDGATHER:
+                window.write_event_value(JOB_BOARDINFO, Update(JOB_BOARDINFO, brawldt))
+                inbrawl = False
+            elif action.task == TASK_GETROUND:
+                window.write_event_value(JOB_ROUNDINFO, (JOB_ROUNDINFO, action))
+            else:
+                pass
