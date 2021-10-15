a98262b448a7caae25e839cf7c8e6f8d42429803
Andrew Rasch <raschykid@gmail.com>
2021-10-15 00:01:03 -0400

    Change pickle file to csv

--- src/stats.py
+++ src/stats.py
@@ -3,7 +3,7 @@ import os.path
 import PySimpleGUI
 import pandas as pd
 
-statsfile = "stats.pkl"
+statsfile = "stats.csv"
 
 
 def update_window(window: PySimpleGUI.Window, hero: str, place: str):
@@ -16,24 +16,21 @@ class PlayerStats:
     def __init__(self, window: PySimpleGUI.Window):
         self.window = window
         if os.path.exists(statsfile):
-            self.df = pd.read_pickle(statsfile)
+            self.df = pd.read_csv(statsfile)
             for row in self.df.itertuples():
                 update_window(self.window, row.Hero, row.Placement)
         else:
             self.df = pd.DataFrame(columns=['Hero', 'Placement'])
-
-    def save(self):
-        try:
-            pd.to_pickle(self.df, statsfile)
-        except Exception as e:
-            print(e)
-
-    def update_stats(self, hero: str, placement: str):
-        self.df = self.df.append({"Hero": hero, "Placement": placement}, ignore_index=True)
-        update_window(self.window, hero, placement)
 
     def export(self, filepath: str):
         try:
             self.df.to_csv(filepath, index=False)
         except:
             pass
+
+    def save(self):
+        self.export(statsfile)
+
+    def update_stats(self, hero: str, placement: str):
+        self.df = self.df.append({"Hero": hero, "Placement": placement}, ignore_index=True)
+        update_window(self.window, hero, placement)
