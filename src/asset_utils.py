a25b9a5bc695af59e6f645ce2853b2072c2f0c2f
Andrew Rasch <raschykid@gmail.com>
2021-10-16 17:09:32 -0400

    LUT updates
    Separate hero and characters
    Add all current heroes to the LUT

--- src/asset_utils.py
+++ src/asset_utils.py
@@ -1,16 +1,49 @@
 import os
 from pathlib import Path
 
-content_id_lookup = {
-    # Heroes
-    "SBB_HERO_MORGANLEFAY": "Morgan Le Fay",
-    "SBB_HERO_DRACULA": "Sad Dracula",
+hero_ids = {
+    "SBB_HERO_PRINCESSBELLE": "Beauty",
+    "SBB_HERO_BIGBADWOLF": "Big Bad Wolf",
+    "SBB_HERO_THECOLLECTOR": "Celestial Tiger",
+    "SBB_HERO_CHARON": "Charon",
+    "SBB_HERO_DARKONE": "Evella",
+    "SBB_HERO_FALLENANGEL": "Fallen Angel",
+    "SBB_HERO_GEPETTO": "Geppetto",
+    "SBB_HERO_OLDGRANNY": "Grandmother",
     "SBB_HERO_GWEN": "Gwen",
+    "SBB_HERO_HORDEDRAGON": "Hoard Dragon",
+    "SBB_HERO_SIRPIPS": "Jack's Giant",
+    "SBB_HERO_KRAMPUS": "Krampus",
+    "SBB_HERO_LOKI": "Loki",
+    "SBB_HERO_MADCATTER": "Mad Catter",
+    "SBB_HERO_MASK": "Mask",
+    "SBB_HERO_MERLIN": "Merlin",
+    "SBB_HERO_KINGLION": "Mihri, King Lion",
+    "SBB_HERO_MORDRED": "Mordred",
+    "SBB_HERO_MORGANLEFAY": "Morgan le Fay",
+    "SBB_HERO_MRSCLAUS": "Mrs. Claus",
+    "SBB_HERO_MUERTE": "Muerte",
+    "SBB_HERO_CARDCHEAT": "Pan's Shadow",
     "SBB_HERO_PETERPAN": "Peter Pants",
+    "SBB_HERO_PIEDPIPER": "Pied Piper",
+    "SBB_HERO_POTIONMASTER": "Potion Master",
+    "SBB_HERO_GANDALF": "Pup the Magic Dragon",
+    "SBB_HERO_DRACULA": "Sad Dracula",
+    "SBB_HERO_SIRGALAHAD": "Sir Galahad",
+    "SBB_HERO_SKIP": "Skip, the Time Skipper",
+    "SBB_HERO_MAIDMARIAN": "Snow Angel",
+    "SBB_HERO_THEGREEDCURSED": "The Cursed King",
+    "SBB_HERO_FATE": "The Fates",
+    "SBB_HERO_MILITARYLEADER": "Trophy Hunter",
+    "SBB_HERO_WONDERWADDLE": "Wonder Waddle",
+    "SBB_HERO_BIGDEAL": "Xelhua",
+}
 
+character_ids = {
     # Characters
     "SBB_CHARACTER_DUMBLEDWARF": "Dubly",
 }
+content_id_lookup = hero_ids | character_ids
 
 
 def get_card_art_name(content_id: str, display_text: str):
