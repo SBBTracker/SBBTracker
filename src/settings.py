04350da91be66efe18b5f510b48be11c0d11d4b7
Andrew Rasch <raschykid@gmail.com>
2022-01-06 21:34:23 -0500

    Change streamable score to off by default

--- src/settings.py
+++ src/settings.py
@@ -54,7 +54,7 @@ turn_display_font_size = Setting("turn-display-font-size", 25)
 # streaming
 streaming_mode = Setting("streaming-mode", False)
 stream_overlay_color = Setting("stream-overlay-color", "#FF00FF")
-streamable_score_list = Setting("streamable-score-list", True)
+streamable_score_list = Setting("streamable-score-list", False)
 streamable_score_max_len = Setting("streamable-score-max-len", 20)
 streamable_scores = Setting("streamable-scores", [])
 
