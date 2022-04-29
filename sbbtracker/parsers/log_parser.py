532bfc68cdb841157b5019a90a8971c835bfbb54
isikdos <ilyas.aricanli@gmail.com>
2022-04-28 20:59:50 -0400

    Update log_parser.py

--- sbbtracker/parsers/log_parser.py
+++ sbbtracker/parsers/log_parser.py
@@ -416,7 +416,7 @@ def run(queue: Queue, log=logfile):
                     lastupdated[action.player1] = current_round
                     lastupdated[action.player2] = current_round
                 elif inbrawl and action.task == TASK_GETROUNDGATHER:
-                    if action.zone in ['Spell', 'Treasure', 'Character', 'Hero']:
+                    if action.zone in ['Spell', 'Treasure', 'Character', 'Hero', 'Hand']:
                         if action.zone == 'Character':
                             if action.slot not in character_slots[action.playerid]:
                                 character_slots[action.playerid].add(action.slot)
